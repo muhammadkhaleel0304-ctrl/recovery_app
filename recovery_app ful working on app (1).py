f3da7d602bd59251f8023d4c6a0eb7db0173f867
muhammadkhaleel0304-ctrl <muhammadkhaleel0304@gmail.com>
2026-03-28 21:04:07 +0500

    Update recovery_app ful working on app (1).py

--- recovery_app ful working on app (1).py
+++ recovery_app ful working on app (1).py
@@ -1,6 +1,6 @@
 import streamlit as st
 import firebase_admin
-from firebase_admin import credentials,
+from firebase_admin import credentials
 if not firebase_admin._apps:
     cred = credentials.Certificate(dict(st.secrets["gcp_service_account"]))
     firebase_admin.initialize_app(cred)
