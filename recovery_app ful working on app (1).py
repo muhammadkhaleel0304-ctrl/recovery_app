9d53d80b154efea67bc88e8457e645634a5178da
Khaleel <muhammadkhaleel0304@gmail.com>
2026-04-09 11:58:37 +0500

    Update recovery_app ful working on app (1).py

--- recovery_app ful working on app (1).py
+++ recovery_app ful working on app (1).py
@@ -4,7 +4,6 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # ================= PAGE CONFIG =================
-st.set_page_config(page_title="Recovery MIS", layout="wide")
 
 st.title("📊 Recovery MIS System")
 
