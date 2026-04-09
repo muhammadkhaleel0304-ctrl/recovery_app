fa362b7398ce5b9572fdb0817ef192ce0f96e06b
Khaleel <muhammadkhaleel0304@gmail.com>
2026-04-09 06:20:09 +0500

    Update recovery_app ful working on app (1).py

--- recovery_app ful working on app (1).py
+++ recovery_app ful working on app (1).py
@@ -1821,8 +1821,6 @@ def save_data(name, df):
 # ================= UPLOAD =================
 st.subheader("📤 Upload Excel")
 
-file = st.file_uploader("Upload Excel File", type=["xlsx"])
-
 if file:
     df = pd.read_excel(file)
     df.columns = df.columns.str.strip()
