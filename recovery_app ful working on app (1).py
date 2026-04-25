f6736821a7137310d9dfbace0597d1033a5858e7
Khaleel <muhammadkhaleel0304@gmail.com>
2026-04-25 15:47:37 +0500

    Update recovery_app ful working on app (1).py

--- recovery_app ful working on app (1).py
+++ recovery_app ful working on app (1).py
@@ -8,43 +8,7 @@ import pandas as pd
 import plotly.express as px
 from fpdf import FPDF
 
-st.title("CNIC QR Generator")
 
-cnic = st.text_input("Enter 13-digit CNIC")
-
-if st.button("Generate QR"):
-    if cnic:
-
-        data = str(cnic).strip()
-
-        # FORCE FULL DATA ENCODING
-        qr = qrcode.QRCode(
-            version=None,  # AUTO size (IMPORTANT FIX)
-            error_correction=qrcode.constants.ERROR_CORRECT_H,
-            box_size=10,
-            border=4
-        )
-
-        qr.add_data(data)
-        qr.make(fit=True)
-
-        img = qr.make_image(fill_color="black", back_color="white")
-
-        buf = BytesIO()
-        img.save(buf, format="PNG")
-
-        img_bytes = buf.getvalue()
-
-        st.image(img_bytes)
-
-        st.download_button(
-            "Download QR",
-            data=img_bytes,
-            file_name="cnic_qr.png",
-            mime="image/png"
-        )
-    else:
-        st.warning("Enter CNIC")
 
 # ----------------- Page config MUST be first -----------------
 
@@ -82,6 +46,43 @@ st.markdown("""
     <hr style='border-top: 3px solid #bbb;'>
 """, unsafe_allow_html=True)
 # -------------------
+st.title("CNIC QR Generator")
+
+cnic = st.text_input("Enter 13-digit CNIC")
+
+if st.button("Generate QR"):
+    if cnic:
+
+        data = str(cnic).strip()
+
+        # FORCE FULL DATA ENCODING
+        qr = qrcode.QRCode(
+            version=None,  # AUTO size (IMPORTANT FIX)
+            error_correction=qrcode.constants.ERROR_CORRECT_H,
+            box_size=10,
+            border=4
+        )
+
+        qr.add_data(data)
+        qr.make(fit=True)
+
+        img = qr.make_image(fill_color="black", back_color="white")
+
+        buf = BytesIO()
+        img.save(buf, format="PNG")
+
+        img_bytes = buf.getvalue()
+
+        st.image(img_bytes)
+
+        st.download_button(
+            "Download QR",
+            data=img_bytes,
+            file_name="cnic_qr.png",
+            mime="image/png"
+        )
+    else:
+        st.warning("Enter CNIC")
 # MDP Section with G/P and Grand Total
 # -------------------
 
