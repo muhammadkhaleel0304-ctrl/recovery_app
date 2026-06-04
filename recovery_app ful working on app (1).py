a1c955e885199418d59e4c3fd4a4a25788449072
Khaleel <muhammadkhaleel0304@gmail.com>
2026-06-04 08:09:19 +0500

    Update recovery_app ful working on app (1).py

--- recovery_app ful working on app (1).py
+++ recovery_app ful working on app (1).py
@@ -322,7 +322,7 @@ else:
     merge_table_placeholder.info("Upload both Merge File and Branch File to generate merged report.")
 F(FPDF):
         def header(self):
- import streamlit as st
+import streamlit as st
 import pandas as pd
 import plotly.express as px
 from fpdf import FPDF
