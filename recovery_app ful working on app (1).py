59272b865b00791a81d337e51430fd523ae63d75
Khaleel <muhammadkhaleel0304@gmail.com>
2026-07-03 18:51:57 +0500

    Update recovery_app ful working on app (1).py

--- recovery_app ful working on app (1).py
+++ recovery_app ful working on app (1).py
@@ -102,9 +102,11 @@ if not st.session_state.login:
             if USERS.get(user) == pwd:
                 st.session_state.login = True
                 st.success("Login successful ✔")
-    st.rerun()
-    except AttributeError:
-    st.experimental_rerun()
+    # Try aur Except dono ka line ke shuru se gap (spaces) barabar hona chahiye
+        try:
+            st.rerun()
+        except AttributeError:
+            st.experimental_rerun()
                 st.error("❌ Invalid username or password")
 
     st.stop()
