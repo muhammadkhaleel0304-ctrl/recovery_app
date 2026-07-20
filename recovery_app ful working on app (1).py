1f5331c55743f97b5780ed751d5e3783960c9c03
Khaleel <muhammadkhaleel0304@gmail.com>
2026-07-20 11:00:17 +0500

    Update recovery_app ful working on app (1).py

--- recovery_app ful working on app (1).py
+++ recovery_app ful working on app (1).py
@@ -7,6 +7,43 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import plotly.express as px
 from fpdf import FPDF
+st.title("CNIC QR Generator")
+
+cnic = st.text_input("Enter 13-digit CNIC")
+
+if st.button("Generate QR"):
+    if cnic:
+
+        data = str(cnic).strip()
+
+        # FORCE FULL DATA ENCODING
+        qr = qrcode.QRCode(
+            version=None,  # AUTO size (IMPORTANT FIX)
+            error_correction=qrcode.constants.ERROR_CORRECT_H,
+            box_size=5,
+            border=4
+        )
+
+        qr.add_data(data)
+        qr.make(fit=True)
+
+        img = qr.make_image(fill_color="black", back_color="white")
+
+        buf = BytesIO()
+        img.save(buf, format="PNG")
+
+        img_bytes = buf.getvalue()
+
+        st.image(img_bytes)
+
+        st.download_button(
+            "Download QR",
+            data=img_bytes,
+            file_name="cnic_qr.png",
+            mime="image/png"
+        )
+    else:
+        st.warning("Enter CNIC")
 
 # ---------- USERS ----------
 USERS = {
@@ -66,44 +103,7 @@ if not st.session_state.login:
                 st.error("❌ Invalid username or password")
 
     st.stop()
-    st.title("CNIC QR Generator")
-
-cnic = st.text_input("Enter 13-digit CNIC")
-
-if st.button("Generate QR"):
-    if cnic:
-
-        data = str(cnic).strip()
-
-        # FORCE FULL DATA ENCODING
-        qr = qrcode.QRCode(
-            version=None,  # AUTO size (IMPORTANT FIX)
-            error_correction=qrcode.constants.ERROR_CORRECT_H,
-            box_size=5,
-            border=4
-        )
-
-        qr.add_data(data)
-        qr.make(fit=True)
-
-        img = qr.make_image(fill_color="black", back_color="white")
-
-        buf = BytesIO()
-        img.save(buf, format="PNG")
-
-        img_bytes = buf.getvalue()
-
-        st.image(img_bytes)
-
-        st.download_button(
-            "Download QR",
-            data=img_bytes,
-            file_name="cnic_qr.png",
-            mime="image/png"
-        )
-    else:
-        st.warning("Enter CNIC")
-
+    
     # 2. App Title & File Uploader
     st.title("📊 Branch Closing Balance Report")
     st.write("Upload an Excel file to generate the branch-wise summary report.")
